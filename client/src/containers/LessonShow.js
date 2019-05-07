af076d5fe073cc7f64072491cbca64391936d346
Eric McKinney <mckinney99@gmail.com>
2019-05-07 15:07:17 -0500

    added lesson show page

--- client/src/containers/LessonShow.js
+++ client/src/containers/LessonShow.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
+import {Col, Row, Container, Button, Card, CardDeck, CardColumns} from 'react-bootstrap'
+
 import '../index.css';
 import { getLessons } from '../actions'
 
@@ -8,26 +10,35 @@ const homeRoute = "/"
 
 function LessonShow(props){
     if (props.lesson === undefined) {
-      return <h1>Loading....</h1>
+      return <h1>Loading.... </h1>
     }
     return (
-      <div>
-        <h1>Lesson Show</h1>
-         <Link to={homeRoute}>Home</Link>
-         <h1>{props.lesson.title}</h1>
-         <h1>{props.lessons}</h1>
-         <button onClick={() => {
-           console.log("A")
-           fetch("http://localhost:3001/api/lessons")
-            .then(res => {
-              console.log("B")
-              return res.json()
-            }).then( data => {
-              console.log("C")
-            })
-            console.log("D")
-         }}>Click Me</button>
-      </div>
+
+
+      <Container class="lesson-show">
+        <h1>{props.lesson.title}</h1>
+
+              <Row className='lesson-item'>
+
+                       <Col lg={12} >
+                       <CardColumns>
+                        <Card>
+                          <Card.Body>
+                            <Card.Title><Link to={`/lessons/${props.lesson.id}`}>{props.lesson.title}</Link></Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">{props.lesson.description}</Card.Subtitle>
+                            <Card.Text>
+                              {props.lesson.body}
+                            </Card.Text>
+                            <Card.Text> By:   {props.lesson.source}</Card.Text>
+                            <p>View more in grade <Card.Link> <Link to={`/grades/${props.lesson.grade}`}>{props.lesson.grade}</Link></Card.Link></p>
+                          </Card.Body>
+                        </Card>
+                       </CardColumns>
+                   </Col>
+
+
+              </Row>
+      </Container>
 )}
 
 // class LessonShow extends React.Component {
